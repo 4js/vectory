d2090e3e81d3c75d173d8297027b46f42bb1dd2b
fengxianqin <fengxianqin@xitcorp.com>
2019-07-15 16:50:27 +0800

    checkbox

--- src/views/resource/cluster/index.vue
+++ src/views/resource/cluster/index.vue
@@ -1,35 +1,32 @@
 <template>
   <div class="cluster-wrapper">
-    <a-row :gutter="16">
-        <a-col :span="8">
-            <a-card title="服务器的状态统计" style="width: 100%">
-               <!-- <a-pie></a-pie> -->
-            </a-card>
-        </a-col>
-        <a-col :span="16">
-            <a-card title="接口交互次数统计" style="width: 100%">
-               <!-- <a-line></a-line> -->
-            </a-card>
-        </a-col>
-    </a-row>
-    <div style="height: 50px"></div>
-    <a-row :gutter="16">
-        <a-col :span="8">
-            <a-card title="扰动点图" style="width: 100%">
-               <!-- <a-point></a-point> -->
-            </a-card>
-        </a-col>
-        <a-col :span="8">
-            <a-card title="仪表图" style="width: 100%">
-               <!-- <a-line></a-line> -->
-            </a-card>
-        </a-col>
-        <a-col :span="8">
-            <a-card title="面积图" style="width: 100%">
-               <!-- <a-line></a-line> -->
-            </a-card>
-        </a-col>
-    </a-row>
+    <a-form :form="form" @submit="handleSubmit" layout="horizontal">
+            <a-form-item label="拥有权限">
+              <a-row :gutter="16" v-for="(permission, index) in permissions" :key="index">
+                <a-col :xl="4" :lg="24">
+                  {{ permission.name }}：
+                </a-col>
+                <a-col :xl="20" :lg="24">
+                  <a-checkbox
+                    v-if="permission.actionsOptions.length > 0"
+                    :indeterminate="permission.indeterminate"
+                    :checked="permission.checkedAll"
+                    @change="onChangeCheckAll($event, permission)">
+                    全选
+                  </a-checkbox>
+                  <a-checkbox-group :options="permission.actionsOptions" v-model="permission.selected" @change="onChangeCheck(permission)" />
+                </a-col>
+              </a-row>
+            </a-form-item>
+            <a-form-item>
+               <a-button
+                type="primary"
+                html-type="submit"
+              >
+                Submit
+              </a-button>
+            </a-form-item>
+          </a-form>
   </div>
 </template>
 
@@ -43,6 +40,85 @@ export default {
     //   APie,
     //   ALine,
     //   APoint
+  },
+  data () {
+    return {
+      form: this.$form.createForm(this),
+      permissions: []
+    }
+  },
+  created () {
+    this.loadPermissions();
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      let _this = this;
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', _this.permissions);
+        }
+      });
+    },
+    onChangeCheck (permission) {
+      permission.indeterminate = !!permission.selected.length && (permission.selected.length < permission.actionsOptions.length)
+      permission.checkedAll = permission.selected.length === permission.actionsOptions.length
+    },
+    onChangeCheckAll (e, permission) {
+      console.log('permission:', permission)
+      Object.assign(permission, {
+        selected: e.target.checked ? permission.actionsOptions.map(obj => obj.value) : [],
+        indeterminate: false,
+        checkedAll: e.target.checked
+      })
+    },
+    loadPermissions () {
+      // getPermissions().then(res => {
+        const result = [
+          {
+            name: '权限1',
+            actionData: [
+              {
+                describe: '新增',
+                action: 'add'
+              },
+              {
+                describe: '删除',
+                action: 'delete'
+              },
+            ]
+          },
+          {
+            name: '权限2',
+            actionData: [
+              {
+                describe: '新增',
+                action: 'add'
+              },
+              {
+                describe: '删除',
+                action: 'delete'
+              },
+            ]
+          },
+        ];
+        this.permissions = result.map(permission => {
+          const options = permission.actionData;
+          permission.checkedAll = false
+          permission.selected = []
+          permission.indeterminate = false
+          console.log('options', options);
+          permission.actionsOptions = options.map(option => {
+            return {
+              label: option.describe,
+              value: option.action
+            }
+          })
+          console.log('permission', permission);
+          return permission
+        })
+      // })
+    }
   }
 }
 </script>
